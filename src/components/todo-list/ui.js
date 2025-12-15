a3704bdd3a66825961d2f7b4ee56b2df635169a1
Zach Joe <jlk.zhou@gmail.com>
2025-12-15 19:50:49 +0900

    Implement adding new todo item function

--- src/components/todo-list/ui.js
+++ src/components/todo-list/ui.js
@@ -1,5 +1,8 @@
 import { renderTodoItem } from "../todo-item/ui";
-import { cancelHandler } from "./ux.js"; 
+import { 
+  cancelButtonHandler, 
+  saveButtonHandler
+} from "./ux.js"; 
 import "./style.css"; 
 
 function renderTodoList(todoList) {
@@ -55,18 +58,72 @@ function renderAddTodoForm() {
   descriptionLabel.textContent = "Todo Description: "; 
   addTodoForm.appendChild(descriptionLabel); 
 
-  // Render the todo title input field
+  // Render the todo description input field
   const descriptionInput = document.createElement("input"); 
   descriptionInput.setAttribute("type", "text"); 
   descriptionInput.setAttribute("name", "description"); 
   descriptionInput.setAttribute("id", "description"); 
   addTodoForm.appendChild(descriptionInput); 
 
+  // Render the label for due date selector
+  const dueDateLabel = document.createElement("label"); 
+  dueDateLabel.setAttribute("for", "due-date"); 
+  dueDateLabel.textContent = "Due Date: "; 
+  addTodoForm.appendChild(dueDateLabel); 
+
+  // Render the due date selector itself
+  const dueDateInput = document.createElement("input"); 
+  dueDateInput.setAttribute("type", "date"); 
+  dueDateInput.setAttribute("name", "due-date"); 
+  dueDateInput.setAttribute("id", "due-date"); 
+  addTodoForm.appendChild(dueDateInput); 
+
+  // Render the label for priority setter
+  const priorityLabel = document.createElement("label"); 
+  priorityLabel.setAttribute("for", "priority"); 
+  priorityLabel.textContent = "Priority: "; 
+  addTodoForm.appendChild(priorityLabel); 
+
+  // Render the priority setter itself
+  const priorityInput = document.createElement("select"); 
+  priorityInput.setAttribute("name", "priority"); 
+  priorityInput.setAttribute("id", "priority"); 
+
+  const noPriority = document.createElement("option"); 
+  noPriority.setAttribute("value", "")
+  noPriority.textContent = "--Select a priority level--"; 
+  priorityInput.appendChild(noPriority); 
+
+  const lowPriority = document.createElement("option"); 
+  lowPriority.setAttribute("value", "2")
+  lowPriority.textContent = "Low"; 
+  priorityInput.appendChild(lowPriority); 
+
+  const mediumPriority = document.createElement("option"); 
+  mediumPriority.setAttribute("value", "1")
+  mediumPriority.textContent = "Medium"; 
+  priorityInput.appendChild(mediumPriority); 
+
+  const highPriority = document.createElement("option"); 
+  highPriority.setAttribute("value", "0")
+  highPriority.textContent = "High"; 
+  priorityInput.appendChild(highPriority); 
+
+  addTodoForm.appendChild(priorityInput); 
+
+  // Render the cancel button
   const cancelButton = document.createElement("button"); 
   cancelButton.classList.add("cancel"); 
   cancelButton.textContent = "Cancel"; 
-  cancelButton.addEventListener("click", cancelHandler); 
+  cancelButton.addEventListener("click", cancelButtonHandler); 
   addTodoForm.appendChild(cancelButton); 
+
+  // Render the save button
+  const saveButton = document.createElement("button"); 
+  saveButton.classList.add("save"); 
+  saveButton.textContent = "Save"; 
+  saveButton.addEventListener("click", saveButtonHandler)
+  addTodoForm.appendChild(saveButton); 
 
   listDiv.appendChild(addTodoForm); 
 }
@@ -74,4 +131,5 @@ function renderAddTodoForm() {
 export { 
   renderTodoList, 
   renderAddTodoButton, 
+  renderAddTodoForm
  }; 
--- src/components/todo-list/ux.js
+++ src/components/todo-list/ux.js
@@ -1,10 +1,43 @@
 import { renderAddTodoButton } from "./ui.js";
+import { TodoItem } from "../todo-item/class.js";
+import { exampleList } from "../../data.js"; 
+import { 
+  renderTodoList, 
+  renderAddTodoForm, 
+} from "./ui.js";
 
-function cancelHandler(event) {
+function cancelButtonHandler(event) {
   event.preventDefault(); 
   const addTodoForm = document.querySelector(".add-todo-form"); 
   addTodoForm.remove(); 
   renderAddTodoButton(); 
 }
 
-export { cancelHandler }; 
+function saveButtonHandler(event) {
+  event.preventDefault(); 
+  const addTodoForm = document.querySelector(".add-todo-form"); 
+  const formData = new FormData(addTodoForm); 
+
+  const title = formData.get("title"); 
+  const description = formData.get("description"); 
+  const dueDate = new Date(formData.get("due-date")); 
+  const priority = formData.get("priority"); 
+
+  const newTodoItem = new TodoItem(
+    title, 
+    description, 
+    dueDate, 
+    priority
+  )
+
+  exampleList.addItem(newTodoItem); 
+
+  renderTodoList(exampleList); 
+  renderAddTodoForm(); 
+
+}
+
+export { 
+  cancelButtonHandler, 
+  saveButtonHandler, 
+}; 
