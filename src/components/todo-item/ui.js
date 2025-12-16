4fceac4048aec5e903e9ac2aeb199f3bb350cb44
Zach Joe <jlk.zhou@gmail.com>
2025-12-17 02:18:36 +0900

    Add wrapper container div for todo item summary

--- src/components/todo-item/ui.js
+++ src/components/todo-item/ui.js
@@ -5,17 +5,15 @@ import {
   detailViewHandler, 
 } from "./ux.js"; 
 
-function renderTodoItem(todoItem) {
-  // Create container for the todo item
-  const containerDiv = document.createElement("div"); 
-  containerDiv.classList.add("todo-item"); 
-  containerDiv.dataset.uuid = todoItem.uuid; 
+function renderSummaryContent(todoItem) {
+  const summaryDiv = document.createElement("div"); 
+  summaryDiv.classList.add("summary"); 
 
   // Render title
   const title = document.createElement("h3"); 
   title.classList.add("title"); 
   title.textContent = todoItem.title;
-  containerDiv.appendChild(title); 
+  summaryDiv.appendChild(title); 
   
   // Render description
   const description = document.createElement("p"); 
@@ -26,7 +24,7 @@ function renderTodoItem(todoItem) {
   description.textContent = `
     ${descriptionText[0]}${descriptionText[1] ? "..." : ""}
   `; 
-  containerDiv.appendChild(description); 
+  summaryDiv.appendChild(description); 
 
   // Render due date
   const dueDate = document.createElement("p"); 
@@ -38,13 +36,26 @@ function renderTodoItem(todoItem) {
   }
   const date = new Intl.DateTimeFormat("en-GB", options).format(todoItem.dueDate); 
   dueDate.textContent = `${date}`; 
-  containerDiv.appendChild(dueDate); 
+  summaryDiv.appendChild(dueDate); 
 
   // Render priority
   const priority = document.createElement("p"); 
   priority.classList.add("priority"); 
   priority.textContent = `Priority: ${todoItem.priority}`; 
-  containerDiv.appendChild(priority); 
+  summaryDiv.appendChild(priority); 
+
+  return summaryDiv; 
+}
+
+function renderTodoItem(todoItem) {
+  // Create container for the todo item
+  const containerDiv = document.createElement("div"); 
+  containerDiv.classList.add("todo-item"); 
+  containerDiv.dataset.uuid = todoItem.uuid; 
+
+  // Render summary of todo
+  const summaryDiv = renderSummaryContent(todoItem); 
+  containerDiv.appendChild(summaryDiv); 
 
   // Render completion status
   const done = document.createElement("p"); 
